a05e3c35f607c0a97cb9e443f47079ef009fc44e
jorge <jorgecuenca.unity@gmail.com>
2021-07-14 22:43:25 -0500

    up django

--- culturameta/culturameta/settings.py
+++ culturameta/culturameta/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'culturameta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'culturameta_db',
+        'USER': 'culturameta',
+        'PASSWORD': 'CultuR4M3t4',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
