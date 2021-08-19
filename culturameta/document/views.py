81779b8e413280a655332663b31420e5f8327fc2
jorge <jorgecuenca.unity@gmail.com>
2021-08-19 16:24:42 -0500

    aas

--- culturameta/document/views.py
+++ culturameta/document/views.py
@@ -29,6 +29,9 @@ class DocumentViewSet(viewsets.ModelViewSet):
             age = self.request.GET.get('year', None)
             module = self.request.GET.get('module', None)
             submodule = self.request.GET.get('submodule',None)
+            if module and age and submodule is not None:
+                queryset = queryset.filter(age=age, module=module, submodule=submodule)
+                return queryset
             if module is not None:
                 queryset = queryset.filter(module=module)
                 return queryset
@@ -41,9 +44,7 @@ class DocumentViewSet(viewsets.ModelViewSet):
             if module and age is not None:
                 queryset = queryset.filter(age=age, module=module)
                 return queryset
-            if module and age and submodule is not None:
-                queryset = queryset.filter(age=age, module=module, submodule=submodule)
-                return queryset
+
 
 
 
