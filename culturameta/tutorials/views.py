ce5edc4d634ec0ad80b7d62647210496a55886df
jorge <jorgecuenca.unity@gmail.com>
2021-08-24 20:16:11 -0500

    add headertoken

--- culturameta/tutorials/views.py
+++ culturameta/tutorials/views.py
@@ -1,9 +1,20 @@
-#menu/viewsets.py
+# menu/viewsets.py
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from .models import Menu
 from .serializers import MenuSerializer
+from ..culturameta.settings import HEADER_TOKEN
+
 
 class MenuViewSet(viewsets.ModelViewSet):
+
+    def create(self, request, *args, **kwargs):
+        request_token = request.headers.get('X-Custom-Header', None)
+        if request_token is None or request_token != HEADER_TOKEN:
+            return Response(status=400)
+        return super(MenuViewSet, self).create(request, *args, **kwargs)
+
     serializer_class = MenuSerializer
 
     def get_queryset(self):
